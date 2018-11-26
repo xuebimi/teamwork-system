f0f016586a707d9c30beef1cdf2cfc19d71456a0
xuebimi <657403288@qq.com>
2018-11-27 00:03:10 +0800

    MXB图标test

---
+++ teamwork/src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div style="padding: 0 5px;background-color: antiquewhite;display: inline-block">
+      <img src="../assets/MXB.png" style="width: 80px;vertical-align:middle">
+      <img src="../assets/MXB.png" style="width: 80px;vertical-align:middle">
+      <span>联合出品</span>
+    </div>
+  </div>
+</template>
